78bdc4e41e16685c51bf4700b947fb1a809cf5b1
Ratnakar Giri <163234593+ratnakargiri973@users.noreply.github.com>
2024-09-08 22:17:37 +0530

    Update App.jsx

--- Frontend/Url-Shortner/src/App.jsx
+++ Frontend/Url-Shortner/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     if (!url) return;
     setLoading(true); 
     try {
-      const res = await axios.post(`http://localhost:3000/newUrl`, {
+      const res = await axios.post(`https://url-shortner-e5ln.onrender.com/newUrl`, {
         url: url
       });
       setShortUrl(res.data.link);
